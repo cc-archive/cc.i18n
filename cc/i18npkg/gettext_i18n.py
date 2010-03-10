6370cb457dbae741ee6e2937786b4dee3560761b
Christopher Allan Webber <cwebber@dustycloud.org>
2010-03-10 15:27:43 -0600

    Make it so you can get a gettext object that matches the current locale

--- cc/i18npkg/gettext_i18n.py
+++ cc/i18npkg/gettext_i18n.py
@@ -1,7 +1,27 @@
+import os
 import gettext
 import pkg_resources
+
 
 MO_PATH = pkg_resources.resource_filename(
     'cc.i18npkg', 'mo')
 I18N_DOMAIN = 'cc_org'
-CCORG_GETTEXT = gettext.translation(I18N_DOMAIN, MO_PATH)
+CCORG_GETTEXT_TRANSLATIONS = {}
+
+
+def translations_for_locale(locale):
+    if CCORG_GETTEXT_TRANSLATIONS.has_key(locale):
+        return CCORG_GETTEXT_TRANSLATIONS[locale]
+
+    # do I have the order backwards here?
+    langs = [locale]
+    if '_' in locale:
+        root_lang = locale.split('_')[0]
+        if os.path.exists(os.path.join(MO_PATH, root_lang)):
+            langs.append(root_lang)
+
+    langs.append('en')
+
+    translations = gettext.translation(I18N_DOMAIN, MO_PATH, langs)
+    CCORG_GETTEXT_TRANSLATIONS[locale] = translations
+    return translations
