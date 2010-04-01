5ddd81edfcc67e50436a18be59beedbb4605d49e
Christopher Allan Webber <cwebber@dustycloud.org>
2010-04-01 12:10:31 -0500

    New version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import sys
 
 setup(
     name = "cc.i18npkg", 
-    version = "0.2.3.1",
+    version = "0.2.3.2",
     packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages = ['cc',],
     include_package_data = True,
