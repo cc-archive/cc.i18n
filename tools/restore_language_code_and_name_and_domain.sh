f14c05114556c212287bccab01feb56186a20557
Christopher Allan Webber <cwebber@dustycloud.org>
2010-07-16 14:14:33 -0500

    Updating the path in restore_language_code_and_name_and_domain.sh

--- tools/restore_language_code_and_name_and_domain.sh
+++ tools/restore_language_code_and_name_and_domain.sh
@@ -2,5 +2,5 @@
 
 # This is even more terrible, but it, too, works.
 
-find ../i18n | grep po$ | grep -v icommons \
+find ../cc/i18n/i18n | grep po$ | grep -v icommons \
  | xargs -l1 python restore_language_code_and_name_and_domain.py
