a4c32b2aa641385da86610d8be843b5ea8feaae4
Christopher Allan Webber <cwebber@dustycloud.org>
2010-11-15 15:23:12 -0600

    Unit test for util.get_well_translated_langs

--- cc/i18n/tests/test_util.py
+++ cc/i18n/tests/test_util.py
@@ -61,3 +61,56 @@ def test_get_all_trans_stats():
     # make sure it cached the old results
     results = util.get_all_trans_stats(transstats)
     assert results == expected
+
+
+def test_get_well_translated_langs():
+    """
+    Test the util.get_well_translated_langs function.
+    """
+    temp_dir = tempfile.mkdtemp()
+
+    # write a fake CSV file
+    transstats = os.path.join(temp_dir, 'transstats.csv')
+    ts_file = file(transstats, 'w')
+    ts_file.write(
+        ('es_AR,564,343,27,60\n'
+         'en_US,564,0,1,0\n'
+         'hr,564,447,17,79\n'
+         'no,564,400,14,70\n'
+         'nl,564,456,16,80\n'
+         'fi,564,460,14,81'))
+    ts_file.close()
+
+    expected_zero = [
+        {'code': 'es_AR', 'name': u'Castellano (AR)'},
+        {'code': 'en_US', 'name': u'English (US)'},
+        {'code': 'hr', 'name': u'hrvatski'},
+        {'code': 'nl', 'name': u'Nederlands'},
+        {'code': 'no', 'name': u'Norsk'},
+        {'code': 'fi', 'name': u'Suomeksi'}]
+    assert util.get_well_translated_langs(0, transstats) == expected_zero
+
+    expected_seventy = [
+        {'code': 'hr', 'name': u'hrvatski'},
+        {'code': 'nl', 'name': u'Nederlands'},
+        {'code': 'no', 'name': u'Norsk'},
+        {'code': 'fi', 'name': u'Suomeksi'}]
+    assert util.get_well_translated_langs(70, transstats) == expected_seventy
+
+    expected_eighty = [
+        {'code': 'nl', 'name': u'Nederlands'},
+        {'code': 'fi', 'name': u'Suomeksi'}]
+    assert util.get_well_translated_langs(80, transstats) == expected_eighty
+
+    # Test caching: overwrite the old file with bogus data, we should
+    # get the old results
+    ts_file = file(transstats, 'w')
+    ts_file.write(
+        ('es_AR,999,999,999,999\n'
+         'en_US,999,999,999,999\n'
+         'hr,999,999,999,999\n'
+         'no,999,999,999,999\n'
+         'nl,999,999,999,999\n'
+         'fi,999,999,999,999'))
+    ts_file.close()
+    assert util.get_well_translated_langs(80, transstats) == expected_eighty
