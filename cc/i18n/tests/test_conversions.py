686355923348c07bb270396069d1ddd57a4b836a
Christopher Allan Webber <cwebber@dustycloud.org>
2011-05-03 15:37:25 -0500

    Tests for ./bin/sync

--- cc/i18n/tests/test_conversions.py
+++ cc/i18n/tests/test_conversions.py
@@ -1,11 +1,11 @@
-import copy
 import os
 import pkg_resources
 import tempfile
+from shutil import copyfile
 
 import polib
 
-from cc.i18n.tools import cc2po, po2cc
+from cc.i18n.tools import cc2po, po2cc, sync
 
 
 FAKE_PO_DIR = pkg_resources.resource_filename(
@@ -14,7 +14,8 @@ FAKE_I18N_DIR = pkg_resources.resource_filename(
     'cc.i18n.tests', 'fake_i18ndir')
 FAKE_MASTER_PO = pkg_resources.resource_filename(
     'cc.i18n.tests', 'fake_master_ccorg.po')
-
+FAKE_MODIFIED_MASTER_PO = pkg_resources.resource_filename(
+    'cc.i18n.tests', 'fake_master_ccorg_modified.po')
 
 def assert_catalogs_match(catalog1, catalog2, cc2po_laziness=False):
     """
@@ -98,3 +99,79 @@ def test_cc_to_po_tool():
         assert_catalogs_match(
             polib.pofile(po_name), polib.pofile(outfile_name),
             True)
+
+
+def test_sync():
+    """
+    Test ./bin/sync (or the function that's run by that)
+
+    This one's a bit tricky, but not too tricky.
+
+    The first time we sync with the normal unchanged master cc_org.po
+    and make sure the files that are written out are pretty much
+    exactly the same as the ones in FAKE_MASTER_PO (they shouldn't
+    have needed to change).  Then we should run the sync again 
+    """
+    outdir = tempfile.mkdtemp()
+    masterpodir = tempfile.mkdtemp()
+
+    # This is so these files don't get adultered by the process
+    fake_master_po = os.path.join(
+        masterpodir, 'fake_master_ccorg.po')
+    fake_master_po_bak = os.path.join(
+        masterpodir, 'fake_master_ccorg.po.bak')
+    fake_master_po_modified = os.path.join(
+        masterpodir, 'fake_master_ccorg_modified.po')
+    fake_master_po_modified_bak = os.path.join(
+        masterpodir, 'fake_master_ccorg_modified.po.bak')
+
+    copyfile(FAKE_MASTER_PO, fake_master_po)
+    copyfile(FAKE_MASTER_PO + '.bak', fake_master_po_bak)
+    copyfile(FAKE_MODIFIED_MASTER_PO, fake_master_po_modified)
+    copyfile(FAKE_MODIFIED_MASTER_PO + '.bak', fake_master_po_modified_bak)
+    
+    # Sync once, presumably files shouldn't be changed
+    sync.sync(FAKE_PO_DIR, outdir, fake_master_po)
+
+    for dirname in os.listdir(FAKE_PO_DIR):
+        po_name = os.path.join(FAKE_PO_DIR, dirname, 'cc_org.po')
+        outfile_name = os.path.join(outdir, dirname, 'cc_org.po')
+        assert_catalogs_match(
+            polib.pofile(po_name), polib.pofile(outfile_name),
+            True)
+
+    sync.sync(outdir, outdir, fake_master_po_modified)
+
+    vi_pofile = polib.pofile(
+        os.path.join(outdir, 'vi', 'cc_org.po'))
+    es_pofile = polib.pofile(
+        os.path.join(outdir, 'es', 'cc_org.po'))
+
+    ## util.name should have changed
+    # was previously untranslated in vi
+    vi_utilname = vi_pofile.find('util.name', by='msgctxt')
+    assert u'fuzzy' not in vi_utilname.flags
+    assert vi_utilname.msgctxt == 'util.name'
+    assert vi_utilname.msgid == 'Namez0rs'
+    assert vi_utilname.msgstr == ''
+
+    # was translated in es
+    es_utilname = es_pofile.find('util.name', by='msgctxt')
+    assert u'fuzzy' in es_utilname.flags
+    assert es_utilname.msgctxt == 'util.name'
+    assert es_utilname.msgid == 'Namez0rs'
+    assert es_utilname.msgstr == 'Nombre'
+
+    ## We removed 'license.mark.curator_help'; assert it is in neither
+    ## catalog
+    assert vi_pofile.find('license.mark.curator_help', by='msgctxt') is None
+    assert es_pofile.find('license.mark.curator_help', by='msgctxt') is None
+
+    ## We added util.whodunnit, which is brand new
+    vi_whodunnit = vi_pofile.find('util.whodunnit', by='msgctxt')
+    es_whodunnit = es_pofile.find('util.whodunnit', by='msgctxt')
+    assert vi_whodunnit.flags == es_whodunnit.flags == []
+    assert vi_whodunnit.msgctxt == es_whodunnit.msgctxt == 'util.whodunnit'
+    assert vi_whodunnit.msgid == es_whodunnit.msgid == 'Whodunnit??'
+    assert vi_whodunnit.msgstr == es_whodunnit.msgstr == ''
+
