5d1180badb91ec22885d1a091004f8bfd1478b7d
Christopher Allan Webber <cwebber@dustycloud.org>
2011-11-01 16:22:39 -0500

    Stupid git updater

---
+++ runcheckouts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Stupid git updater
+# 
+# Written in 2011 by Christopher Allan Webber, Creative Commons
+# 
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+# 
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+if [ ! -d checkouts ]; then
+    mkdir checkouts
+fi
+    
+cd checkouts    
+
+function make_or_update_checkout
+{
+    DIRNAME=$1
+    URL=$2
+
+    if [ ! -d $DIRNAME ]; then
+        echo "-- Cloning into $DIRNAME --"
+        git clone $URL $DIRNAME
+    else
+        echo "-- Updating $DIRNAME --"
+        cd $DIRNAME
+        git pull
+        cd ..
+    fi
+}
+
+make_or_update_checkout cc.engine git://code.creativecommons.org/cc.engine.git
+make_or_update_checkout cc.license git://code.creativecommons.org/cc.license.git
+make_or_update_checkout cc.licenserdf git://code.creativecommons.org/license.rdf.git
+make_or_update_checkout cc.deedscraper git://code.creativecommons.org/deedscraper.git
