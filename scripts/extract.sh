9ee7f0759bed2701bb3d69e76447f1be7fd440bd
Rob Myers <rob@robmyers.org>
2017-04-06 17:37:16 -0700

    Update extract script to use buildout version of pybabel.

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -3,6 +3,6 @@
 CWD=`pwd`
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-pybabel extract -F babel.ini -o cc/i18n/po/en/cc_org.po .
+bin/pybabel extract -F babel.ini -o cc/i18n/po/en/cc_org.po .
 
 cd "${CWD}"
