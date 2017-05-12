b9d7028bf14104a8109a062ec5ce8ffe2482cf85
Rob Myers <rob@robmyers.org>
2017-05-12 11:22:24 -0700

    Use venv version of pybabel.

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -3,6 +3,6 @@
 CWD=`pwd`
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-bin/pybabel extract -F babel.ini -o cc/i18n/po/en/cc_org.po .
+pybabel extract -F babel.ini -o cc/i18n/po/en/cc_org.po .
 
 cd "${CWD}"
